f33a6420ada6c07c2e8513b65995a4afc43de01e
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-08-15 16:12:57 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+
+//task 22 : intentional error
+let car = `subaru`;
+
+console.log("Is car == `subaru`?I predict True.")
+console.log(car == `subaru`)//true
+
+console.log("Is car == `honda`?I predict false.")
+console.log(car = `honda`)//False
+
+console.log("Is carlenght == 7? I predict true.");
+console.log("carlenght == 7"); //true
+
+console.log("Is carlenght == 6? I predict False.")
+console.log("carlenght == 6"); //false
+
+console.log("Is car toUpperCase() == `SUBARU`? I predict true.")
+console.log("car toUpperCase() == `SUBARU`,") //TRUE
+
+console.log("Is car toUpperCase() == `HONDA`? I predict false.")
+console.log("car toUpperCase == `HONDA`") // false
+
+console.log("is car includes = (`sub`)? I predict true.")
+console.log("car includes(`sub`) ") //true
+
+console.log("is car includes = (`hon`)? I predict false.")
+console.log("car includes(`hon`) ") //false
+
+console.log("is car includes = (`aru`)? I predict true.")
+console.log("car includes(`aru`) ") //true
+
+console.log("is car includes = (`da`)? I predict false.")
+console.log("car includes(`do`) ") //false
